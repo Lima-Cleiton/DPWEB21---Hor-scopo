b9a015830339a7e8cb2268b3c17117913d9f9e07
Lima-Cleiton <cleiton-s-l@hotmail.com>
2021-12-13 14:41:07 -0300

    Segundo commit

---
+++ Horóscopo/home.js
@@ -0,0 +1,25 @@
+function conteudoAries(){
+    document.getElementById("modalaries").style.display = "block"
+}
+
+
+function fecharAries(){
+    document.getElementById("modalaries").style.display = "none"
+}
+
+function conteudoCancer(){
+    document.getElementById("modalcancer").style.display = "block"
+}
+
+
+function fecharCancer(){
+    document.getElementById("modalcancer").style.display = "none"
+}
+
+function conteudoEscorpiao(){
+    document.getElementById("modalescorpiao").style.display = "block"
+}
+
+function fecharEscorpiao(){
+    document.getElementById("modalescorpiao").style.display = "none"
+}
